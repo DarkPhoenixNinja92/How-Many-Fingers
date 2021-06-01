0d576c689343e1e30f09f3b2f3042868706cf28e
Shaun Pour <scpour@gmail.com>
2021-06-01 14:05:02 -0400

    1.0

---
+++ js/Fingers.js
@@ -0,0 +1,24 @@
+function fingerNum() {
+    let userInput = parseFloat(prompt("Please enter a number."));
+    let genInput = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    let count = 0;
+    while (Number.isNaN(userInput) == true) {
+        alert("Invalid value! Please try again.");
+        userInput = parseFloat(prompt("Please enter a number."));
+        if (count >= 3) {
+            alert("Too many wrong guesses! Changing the number.");
+            genInput = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+        }
+    }
+    while (userInput != genInput) {
+        if (userInput > genInput) {
+            alert("Incorrect. Guess was too high!");
+        } else {
+            alert("Incorrect. Guess was too low!");
+        }
+        userInput = parseFloat(prompt("Please enter a number."));
+        count += 1;
+    }
+    alert("Great job! You got it right.");
+}
+fingerNum();
